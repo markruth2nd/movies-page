45a14cc3c4cb9f7755e152bedc44a7ee24c7045d
markruth2nd <119530770+markruth2nd@users.noreply.github.com>
2023-03-22 18:58:04 +0000

    api code added, just need to add comments throughout code

---
+++ src/components/Ombd.js
@@ -0,0 +1,56 @@
+import React, { useState, useEffect } from "react";
+
+import MovieCard from "../MovieCard";
+import SearchIcon from "../search.svg";
+
+import "../app.css";
+
+const API_URL = 'http://www.omdbapi.com/?apikey=fbdf1d80'
+
+
+const Omdb = () => {
+    const [movies, setMovies ] = useState([]);
+    const [searchTerm, setSearchTerm] = useState("");
+
+    useEffect(() => {
+        searchMovies("Spiderman");
+      }, []);
+
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+
+        setMovies(data.Search);
+    }
+
+    return (
+        <div className="app">
+            <h1>MovieLand</h1>
+
+            <div className="search">
+                <input 
+                placeholder="Search for Movies" 
+                value={searchTerm} 
+                onChange={(e) => setSearchTerm(e.target.value)} />
+
+                <img src={SearchIcon} alt="search" 
+                onClick={() => searchMovies(searchTerm)}/>
+            </div>
+
+        {movies?.length > 0
+            ? (
+            <div className="container">
+            {movies.map((movie) => (
+                <MovieCard movie={movie} />
+            ))}
+            </div>
+            ) : (
+                <div className="empty">
+                    <h2>No Movies Found</h2>
+                </div>
+            )}
+            </div>
+    );
+};
+
+export default Omdb

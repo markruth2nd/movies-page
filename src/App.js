4696284d04a4ddd2b07542103238b14a5206def7
Yusuf <yusf.mbk@gmail.com>
2023-03-24 19:27:46 +0000

    added blank gamescard.jsx to code later and also finished coding the App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Omdb from "./component/Omdb";
+import Comedies from './pages/Comedies';
+import Anime from './pages/Anime';
+import MovieRequest from './pages/MovieRequest';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    return (
+    <Router>
+        <Routes>
+            <Route path="/" element={<Omdb />} />
+            <Route path="/comedies" element={<Comedies />} />
+            <Route path="/anime" element={<Anime />} />
+            <Route path="/movierequest" element={<MovieRequest />} />
+        </Routes>
+    </Router>
+)}
 
 export default App;

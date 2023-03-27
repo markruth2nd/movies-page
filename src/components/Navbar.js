d953d581b5f252d931f7a81959603605d06db1f2
Ric Johnston <119433069+ricJ14@users.noreply.github.com>
2023-03-27 20:51:34 +0100

    added functionality to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,14 @@
-import React from "react";
+import React, { Component } from "react";
 import "../styles/Navbar.css";
 
 
-function Navbar() {
+class Navbar extends Component {
+    state = { clicked: false };
+    handleClick = () => {
+        this.setState({ clicked: !this.state.clicked })
+    }
+
+    render() {
     return (
        <>
         <nav>
@@ -10,15 +16,19 @@ function Navbar() {
             <a href="index.html">Search4Movies</a>
             </div>
             <div>
-                <ul id="navbar">
+                <ul id="navbar" className={this.state.clicked ? "#navbar active" : "#navbar"}>
                     <li><a className="active" href="index.html">Comedies</a></li>
                     <li><a href="index.html">Anime</a></li>
                     <li><a href="index.html">Movie Request</a></li>
                 </ul>
             </div>
+            <div id="mobile" onClick={this.handleClick}>
+                <i id="bar" className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}></i>
+            </div>
         </nav>
        </>
     )
+}
 }
 
 export default Navbar;

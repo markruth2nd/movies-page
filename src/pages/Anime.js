c4ff458745c1fe7983998dc79c694709a6e27a7b
MarlenaDowner <119687938+MarlenaDowner@users.noreply.github.com>
2023-03-28 19:08:42 +0100

    semi finished

---
+++ src/pages/Anime.js
@@ -0,0 +1,56 @@
+import React, { useState, useEffect } from "react";
+
+
+import MovieCard from "../MovieCard";
+import whiteSearch from "../images/whiteSearch.svg";
+
+import "../app.css";
+
+const API_URL = 'http://www.omdbapi.com/?apikey=fbdf1d80'
+
+
+const Anime = () => {
+    const [movies, setMovies ] = useState([]);
+    const [searchTerm, setSearchTerm] = useState("");
+
+    useEffect(() => {
+        searchMovies("Dragon Ball Z");
+      }, []);
+
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+
+        setMovies(data.Search);
+    }
+
+    return (
+        <div className="app">
+
+            <div className="container">
+                <input
+                id="movieSearh"
+                placeholder="Type Movie..." 
+                value={searchTerm} 
+                onChange={(e) => setSearchTerm(e.target.value)} />
+                <img src={whiteSearch} id="Icon" alt="Icon" 
+                onClick={() => searchMovies(searchTerm)}/>
+            </div>
+
+        {movies?.length > 0
+            ? (
+            <div className="movieContainer">
+            {movies.map((movie) => (
+                <MovieCard movie={movie} />
+            ))}
+            </div>
+            ) : (
+                <div className="empty">
+                    <h2>Movie Not Found</h2>
+                </div>
+            )}
+            </div>
+    );
+};
+
+export default Anime
--- src/pages/MovieRequest.js
+++ src/pages/MovieRequest.js
@@ -1,1 +1,71 @@
-   
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+import logoRequestPage from "../images/logo.png";
+
+const MovieRequest = () => {
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm(
+            'service_kllagzy',
+            'template_2k7tzik',
+            form.current,
+            '3xIWOQx_cbX53gitG')
+            .then((result) => {
+                console.log(result.text);
+                console.log("Message sent, I'll reply shortly")
+            }, (error) => {
+                console.log(error.text);
+            });
+    };
+
+    return <div className="">
+        <div className="logoRequestPage">
+            <a href={"/"} className="">
+                <img className="logoRequestPage" src={logoRequestPage} alt="logoRequestPage" />
+                </a>
+                </div>
+                <h1 className="requestHeader">
+                    Movie Request
+                </h1>
+                <p className="requestInfo">
+                    Here at Search4movies we aim to deliver the best service to all our fans. If you cant find your favorite Movie, Anime, or Game feel free to drop us an email with any Movie Request!
+                </p>
+        <form ref={form} onSubmit={sendEmail}>
+            <label>Name</label>
+            <input type="text" name="user_name" />
+            <label>Email</label>
+            <input type="email" name="user_email" />
+            <label>Message</label>
+            <textarea name="message" />
+            <input type="submit" value="Send" />
+        </form> 
+        {/* This really should be there but cant figure out why the back ground cuts page in half */}
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+    </div>
+
+
+}
+
+export default MovieRequest;
